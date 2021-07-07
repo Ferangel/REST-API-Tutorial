6883d55b5e71bab7c5ec83aeb945adb4b3ac06f7
ferangel <fer.quaglietta@gmail.com>
2021-07-07 16:52:33 +0200

    enabled cors

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,25 @@ import config from "config";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
+import cors from "cors"
 import { deserializeUser } from "./middleware";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
-app.use(deserializeUser);
+var corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+app.use(deserializeUser);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.options("*", (req, res) => {
+    res.status(200).send("Preflight request allowed");
+  });
 
 app.listen(port, host, () => {
   log.info(`Server listing at http://${host}:${port}`);
